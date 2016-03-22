734dbb8de4cd3e5c763862a13304ef3e0e3116d1
Brandon Hanabarger <animalxingguard@gmail.com>
2016-03-22 16:31:08 -0700

    Assignment 6
    Character controller, double jump, test room

---
+++ Project 2/Project 2 - 2D Platformer/Assets/_scripts/characterMove.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterMove : MonoBehaviour {
+
+	Rigidbody2D charRB;
+	Transform charTransform;
+	float hFactor;
+	float vVelocity;
+	public float jumpVal;
+	public float speed;
+	public bool onGround = true;
+	public int jumps;
+
+
+	// Use this for initialization
+	void Start () {
+
+		charTransform = gameObject.transform;
+		charRB = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Horizontal Movement
+		hFactor = Input.GetAxis ("Horizontal") * speed;
+		charTransform.position = new Vector2 (hFactor + charTransform.position.x, charTransform.position.y);
+
+		//Vertical Movement
+		if (Input.GetKeyDown (KeyCode.Space) && onGround == true && jumps == 0) { //Checks for Space key and limits number of jumps
+			jumps++;
+			vVelocity = jumpVal;
+		} else if (Input.GetKeyDown (KeyCode.Space) && jumps == 1) {
+			jumps++;
+			vVelocity = jumpVal;
+
+		} else {
+			vVelocity = 0;
+		}
+		charRB.velocity += new Vector2 (0, vVelocity);
+
+
+	}
+
+	//Resets jumps
+	void OnTriggerEnter2D(Collider2D col){
+		if(col.CompareTag("Ground")){
+			Debug.Log ("Touched Ground");
+			onGround = true;
+			jumps = 0;
+		}
+
+	}
+
+	//Toggles onGround
+	void OnTriggerExit2D(Collider2D col){
+		if(col.CompareTag("Ground")){
+			Debug.Log ("Left Ground");
+			onGround = false;
+		}
+
+	}
+}
